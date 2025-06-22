af5a860f3018470cee24728da6c04728db362f91
Manas0410 <manasshrivastava0410@gmail.com>
2025-06-22 14:29:55 +0530

    server cloned

--- src/Users/Components/RHFUsersProvider.tsx
+++ src/Users/Components/RHFUsersProvider.tsx
@@ -14,6 +14,7 @@ const RHFUsersProvider = () => {
     <FormProvider {...methods}>
       <Users />
       <DevTool control={methods.control} />
+      {/* watch is also there */}
     </FormProvider>
   );
 };
